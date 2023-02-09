58732b8358b00fa9ff20a4ed3636acfeee544ee3
rhodyapps <12385176+rhodyapps@users.noreply.github.com>
2023-02-09 14:53:00 -0500

    Create hello-world.cpp

---
+++ c-programs/hello-world.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/* 
+Use this program to test you VS Code C compiler
+*/
+
+int main() {
+   // printf() displays the string inside quotation
+   printf("Hello, World!\n");
+   return 0;
+}
