3b09fb457cf15ad90b019b0bfb5f8305acdb94f9
rhodyapps <12385176+rhodyapps@users.noreply.github.com>
2023-02-09 14:12:16 -0500

    Update caesars-cipher.cpp

--- c-programs/caesars-cipher.cpp
+++ c-programs/caesars-cipher.cpp
@@ -1,50 +1,27 @@
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
-#include <ctype.h>
+#include<iostream>
+#include<string>
 
-enum {MAX_LEN = 1024};
+/* 
 
-int main(void) {
-    size_t len;
-    int shift;
-    int key=13;
-    char *text;
-    char num[16];
+E_n(x)=(x+n)mod\ 26           
+(Encryption Phase with shift n)
 
-    text = malloc(MAX_LEN);
-    if (text == NULL) {
-        perror("malloc");
-        exit(EXIT_FAILURE);
+D_n(x)=(x-n)mod\ 26           
+(Decryption Phase with shift n)
+*/
+
+using namespace std;
+int main(){
+    int i,j,k;
+    string s,t;
+    int key;
+    cout<<"\nEnter the key: ";
+    cin>>key;
+    cout<<"\nEnter the message: ";
+    cin>>s;
+    for(i=0;i<s.size();i++){
+        t+=(s[i]-'A'+key)%26+'A';
     }
-
-    printf("Input text to be encrypted (lowercase): ");
-    fflush(stdout);
-    if (fgets(text, MAX_LEN, stdin) == NULL)
-        exit(EXIT_FAILURE);
-
-    len = strlen(text);
-    if (text[len - 1] == '\n')
-        text[len - 1] = '\0';
-    len -= 1;
-
-    printf("Choose shift number [1-26]: ");
-    fflush(stdout);
-    if (fgets(num, 16, stdin) == NULL)
-        exit(EXIT_FAILURE);
-
-    shift = (int) strtol(num, NULL, 0);
-    if (shift < 1 || shift > 26) {
-        fprintf(stderr, "Shift number is out of range");
-        exit(EXIT_FAILURE);
-    }
-
-    for (int i = 0; i < len; ++i) {
-        if (!isspace(text[i]) || !isblank(text[i]))
-            printf("%c", (((text[i] - 97)+shift)%26)+97);
-        else
-            printf("%c", text[i]);
-    }
-
-    exit(EXIT_SUCCESS);
+    cout<<"\n\nEncrypted message: "<<t<<'\n';
+    return 0;
 }
