1e0e8b9ded906a0289bc5e8568c766500640ed01
rhodyapps <12385176+rhodyapps@users.noreply.github.com>
2024-03-17 16:17:59 -0400

    Create c-to-asm.c

---
+++ c-programs/c-to-asm.c
@@ -0,0 +1,15 @@
+// C code stored in c-to-asm.c file
+#include <stdio.h>
+ 
+// global string
+char s[] = "Hello RWU!";
+ 
+// Driver Code
+int main()
+{
+    // Declaring variables
+    int a = 2000, b =24;
+     
+    // Printing statement
+    printf("%s %d \n", s, a+b);
+}
